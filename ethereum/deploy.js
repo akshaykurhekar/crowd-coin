06434fe89c939eeca37876b0e90852e53a65693a
akshaykurhekar <akshay.kurhekar1014662@gmail.com>
2022-01-20 06:26:43 +0530

    deploy file refactor

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -3,15 +3,14 @@ const Web3 = require("web3");
 const CampaignFactory = require("./build/CampaignFactory.json");
 
 const provider = new HDWalletProvider(
-  "horse neither robot throw begin luggage depth fragile bulb artist august gate",
-  "https://ropsten.infura.io/v3/cc2b3d7b2a4642baa7f5ac5526a1dfc5"
+  "add here your 12 words key of metamask",
+  "add here your infura network link"
 );
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-
-  //   console.log('Attempting to deploy from account', accounts[0]);
 
   const result = await new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface)
